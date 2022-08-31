5a2487a9dc361e4e78daf35b6b698bf22feda636
evangelinemills <eomills.mills@gmail.com>
2022-08-31 23:58:40 +0000

    create snake move function and key down functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,23 @@
 const canvas = document.getElementById("game-area");
 const ctx = canvas.getContext("2d");
 
+// Snake display
+let tileCount=20;
+let tileSize=18;
+let headX=10;
+let headY=10;
+
+// Snake speed
+let xvelocity=0;
+let yvelocity=0;
+
+
 function drawGame () {
     clearScreen();
 }
 
 function clearScreen() {
-    ctx.fillStyle= 'green';
+    ctx.fillStyle= '#303a2b';
     ctx.fillRect(0, 0, canvas.clientWidth, canvas.clientHeight);
 }
 
@@ -18,16 +29,52 @@ function drawGame() {
     setTimeout(drawGame, 1000/speed);
 }
 
-let tileCount=20;
-let tileSize=18;
-let headX=10;
-let headY=10;
-
 function drawGame() {
     clearScreen();
     drawSnake();
 }
+
 function drawSnake() {
-    ctx.fillStyle= 'pink';
+    ctx.fillStyle= '#ff99c9';
     ctx.fillRect(headX* tileCount,headY* tileCount, tileSize,tileSize);
 }
+
+document.body.addEventListener('keydown', keyDown);
+
+function keyDown(event) {
+
+    //Up
+    if (event.keyCode==38){
+        yvelocity=-1;
+        xvelocity=0;
+    }
+    //Down
+    if (event.keyCode==40){
+        yvelocity=1;
+        xvelocity=0;
+    }
+
+    //Left
+    if (event.keyCode==37){
+        yvelocity=0;
+        xvelocity=-1;
+    }
+
+    //Right
+    if (event.keyCode==39){
+        yvelocity=0;
+        xvelocity=1;
+    }
+}
+
+function changeSnakePosition() {
+    headX = headX + xvelocity;
+    headY = headY + yvelocity;
+}
+
+function drawGame() {
+    clearScreen();
+    drawSnake();
+    changeSnakePosition();
+}
+
