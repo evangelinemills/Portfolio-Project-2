13c532d5a57d27972e7f895acb481452dafbaa58
evangelinemills <eomills.mills@gmail.com>
2022-08-31 10:07:23 +0000

    set up game canvas background and draw function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,19 @@
 const canvas = document.getElementById(game-area);
 const ctx = canvas.getContext("2d");
+
+function drawGame () {
+    clearScreen();
+}
+
+function clearScreen() {
+    ctx.fillStyle= "#303a2b"
+    ctx.fillRect(0, 0, canvas.clientWidth, canvas.clientHeight)
+}
+
+drawGame();
+
+function drawGame() {
+    let speed = 7;
+
+    setTimeout(drawGame, 1000/speed);
+}
