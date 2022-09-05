62e53ece1dc89186d465e11a6bdc3998d2698f7a
evangelinemills <eomills.mills@gmail.com>
2022-09-05 10:47:52 +0000

    add game over function and for loops for collision

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,25 @@
 const canvas = document.getElementById("game-area");
 const ctx = canvas.getContext("2d");
 
+class snakePart {
+    constructor(x, y){
+        this.x=y;
+        this.y=y;
+    }
+}
+
 
 let speed = 7;
+let tileCount = 20;
 
-let tileCount=canvas.clientWidth/tileCount-2;
-let tileSize=18;
+let tileSize=canvas.clientWidth/tileCount-2;
 
 let headX=10;
 let headY=10;
 
 // Snake Parts
+const snakeParts=[];
+let tailLength = 2;
 
 // Snake speed
 let xvelocity=0;
@@ -27,9 +36,63 @@ let score = 0;
 
 
 function drawGame () {
+    changeSnakePosition();
+    
+    let result = isGameOver();
+    if(result) {
+        return;
+    }
+
     clearScreen();
     drawSnake();
     drawFood();
+
+    chackCollision()
+    drawScore();
+    setTimeout(drawGame, 1000/speed);
+}
+
+//Game Over
+function isGameOver() {
+    let gameOver = false;
+//check game commence
+    if(yvelocity===0 && xvelocity===0) {
+        return false;
+    }
+
+    if(headX<0) { //if hits left wall
+        gameOver = true;
+    }
+
+    else if(headX === tileCount) { //if hits right wall
+        gameOver = true;
+    }
+
+    else if(headY<0) {//if hits top wall
+        gameOver = true;
+    }
+
+    else if(headY===tileCount) {//if hits bottom wall
+        gameOver = true;
+    }
+
+    //if hits own body
+
+    for(let i=0, i<snakeParts.length, i++){
+        let part=snakeParts[i];
+        if(part.x=== headX && part.y === headY) {
+            gameOver = true;
+            break;
+        }
+    }
+
+    if(gameOver) {
+        ctx.fillStyle="#c1bddb";
+        ctx.font="60px 'Bangers'";
+        ctx.fillText("Game Over!!", canvas.clientWidth/6.5, canvas.clientHeight/2)
+    }
+
+    return gameOver;
 }
 
 function clearScreen() {
@@ -42,7 +105,7 @@ drawGame();
 function drawGame() {
     let speed = 7;
 
-    setTimeout(drawGame, 1000/speed);
+    
 }
 
 function drawGame() {
@@ -52,7 +115,24 @@ function drawGame() {
 // Draw Snake Function
 function drawSnake() {
     ctx.fillStyle= '#ff99c9';
-    ctx.fillRect(headX* tileCount,headY* tileCount, tileSize,tileSize);
+    for(let i=0, i<snakeParts.length, i++){
+        let part = snakeParts[i]
+         ctx.fillRect(part.x *tileCount, part.y *tileCount, tileSize, tileSize)
+    }
+   //increase snake length
+   snakeParts.push(new snakePart(headX, headY));
+   if(snakeParts.length>tailLength) {
+        snakeParts.shift();
+    } 
+    ctx.fillStyle="#c1bddb";
+    ctx.fillRect(headX* tileCount, headY* tileCount, tileSize, tileSize)
+}
+
+
+// snake position function
+function changeSnakePosition() {
+    headX = headX + xvelocity;
+    headY = headY + yvelocity;
 }
 
 // Draw Food Function
